4a499d95d088737738060ff8b61b5d2ea17e5d1b
devRena <rnkontogianni@yahoo.gr>
2019-03-27 20:10:40 +0000

    Added comments and generic error handling

--- server.js
+++ server.js
@@ -10,11 +10,23 @@ app.use(
 );
 app.use(bodyParser.json());
 
+// Custom error handling
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log("Invalid Request");
+    res.status(400).send("Invalid Request");
+  } else {
+    next();
+  }
+});
+
+// Default get / page 
 app.get("/", function(req, res) {
   res.send("Welcome to Rena's 5app Test");
 });
 
-app.post("/api/", function(req, res) {
+// Filter endpoint
+app.post("/api/filter", function(req, res) {
   checkRequest(req.body, res, "Body is missing in your request");
   checkRequest(
     req.body.payload,
@@ -43,15 +55,17 @@ app.post("/api/", function(req, res) {
   res.send(result);
 });
 
-
-const port = process.env.PORT || 80
+// Select port
+const port = process.env.PORT || 80;
 const server = app.listen(port);
 console.log("Port is: " + port);
 
+// Request Validation
 function checkRequest(parameter, res, errorMessage) {
   if (!parameter) res.status(400).send(errorMessage);
 }
 
+// Select correct thumbnail url
 function getCorrectThumbnail(thumbnail) {
   const correctLogo = thumbnail.logos.find(logo => {
     // 64x64 => [64,64]
@@ -70,6 +84,7 @@ function getCorrectThumbnail(thumbnail) {
   return correctLogo ? correctLogo.url : "";
 }
 
+// Check if size of logo is correct
 function checkSize(size) {
   return size >= 64 && size <= 128;
 }
