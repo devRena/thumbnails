b7a1576b57011c771e18d36da4f26187636975ab
devRena <rnkontogianni@yahoo.gr>
2019-03-27 17:34:38 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# 5app-coding-challenge
+
+Please create a webservice on a public URL which modifies an incoming JSON `POST` payload and returns a response. 
+
+The response should be a JSON object with property `response` with an Array of items.
+
+i.e...
+```
+{
+   response: [{name, count, thumbnail}, ...]
+}
+```
+
+- Where `name` and `count` match their respective properties from the payload
+- The payload is filtered, so only items with a `count` greater than `1` are returned.
+- The `thumbnail` is a `url` selected from the payload item's list of `logos` no larger than 128x128 but no smaller than 64x64.
+
+Please see [/samples](/samples) for example request and response
+
+# Submit
+
+To submit your service please enter the details in https://fiveapp-coding-challenge.herokuapp.com
---
+++ server.js
@@ -0,0 +1,69 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var util = require("util");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+
+app.post("/", function(req, res) {
+  checkRequest(req.body, res, "Body is missing in your request");
+  checkRequest(
+    req.body.payload,
+    res,
+    "Can not find payload array in your request"
+  );
+  checkRequest(util.isArray(req.body.payload), res, "Payload is not an array");
+
+  const items = req.body.payload;
+
+  const itemsFiltered = items
+    .filter(item => item.count && item.count > 1)
+    .map(item => {
+      const newItem = {
+        name: item.name,
+        count: item.count,
+        thumbnail: getCorrectThumbnail(item)
+      };
+      return newItem;
+    });
+
+  const result = {
+    response: itemsFiltered
+  };
+
+  res.send(result);
+});
+
+console.log("Port is 8080");
+var server = app.listen(8080);
+
+function checkRequest(parameter, res, errorMessage) {
+  if (!parameter) res.status(400).send(errorMessage);
+}
+
+function getCorrectThumbnail(thumbnail) {
+  const correctLogo = thumbnail.logos.find(logo => {
+    // 64x64 => [64,64]
+    const sizeArray = logo.size.split("x");
+    if (
+      sizeArray.length === 2 &&
+      checkSize(sizeArray[0]) &&
+      checkSize(sizeArray[1])
+    ) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return correctLogo ? correctLogo.url : "";
+}
+
+function checkSize(size) {
+  return size >= 64 && size <= 128;
+}
